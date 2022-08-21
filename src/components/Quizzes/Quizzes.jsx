c1b75ff9d08b9c9c687c6cd3b63401af2022640b
MoatazNasr <75985257+MoatazNasr@users.noreply.github.com>
2022-08-21 12:30:50 +0200

    Update Quizzes.jsx

--- src/components/Quizzes/Quizzes.jsx
+++ src/components/Quizzes/Quizzes.jsx
@@ -30,7 +30,7 @@ const Quizzes = ({setFullscreenElement}) => {
         <ArrowBackIcon fontSize="large" />
       </NavLink>
       <h1 className="labs-headline">
-        {location.state.chapterName + "Pop Quiz"}
+        {location.state.chapterName + " Pop Quizzes"}
       </h1>
       <div className="class-labs ">
         {quizzes.length > 0
